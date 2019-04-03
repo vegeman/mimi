3e0e6c12a83d6ef88abab061e94ab23383d16151
Vege Lin <1802862@starlux-airlines.com>
2019-04-03 14:22:28 +0800

    set view

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ let loop = false;
 		// devtools: true
 	});
 	const page = await browser.newPage();
+	page.setViewport({
+		width : 1200,
+		height : 800
+	})
 	await page.goto('https://account.xiaomi.com/pass/serviceLogin?callback=http%3A%2F%2Fbuy.mi.com%2Ftw%2Flogin%2Fcallback%3Ffollowup%3Dhttps%253A%252F%252Fwww.mi.com%252Ftw%252F%26sign%3DZGY3MjQzOTBlOTU1OWE1ODBhNDUxM2NmNDliMDkzZGEyZDIyODVlNA%2C%2C&sid=mi_xiaomitw&_locale=zh_TW&checkSafePhone=false');
 	await page.type('#username', process.env.ACCOUNT)
 	await page.type('#pwd', process.env.PASSWORD)
